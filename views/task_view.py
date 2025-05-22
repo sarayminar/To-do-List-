86fc5bf659a4d85138dcbce3bb76028e40bbd111
sarayminar <sarayminar@gmail.com>
2025-05-22 14:55:39 +0200

    fixing date

--- views/task_view.py
+++ views/task_view.py
@@ -10,14 +10,14 @@ def mostrar_todas_tareas(tareas):
             for tarea in tareas:
                 estado = Fore.GREEN +  "✅ Completada" if tarea.status else Fore.RED + "❌ No completada"
                 cat = tarea.category if tarea.category else Fore.RED + "Sin categoria";
-                print(Fore.CYAN + f"🆔 {tarea.id} - {tarea.title} - Usuario: {tarea.user}: descripcion: {tarea.description}, categoria: [{cat}], prioridad: {tarea.priority}, fecha de creación [{tarea.createDate}], fecha de edición [{tarea.lastEditDate}], [{estado}].")
+                print(Fore.CYAN + f"🆔 {tarea.id} - {tarea.title} - Usuario: {tarea.user}: descripcion: {tarea.description}, categoria: [{cat}], prioridad: {tarea.priority}, fecha de creación [{tarea.createDate.strftime("%Y-%m-%d %H:%M:%S")}], fecha de edición [{tarea.lastEditDate.strftime("%Y-%m-%d %H:%M:%S")}], [{estado}].")
     else:
         print(Fore.YELLOW + "📭 No hay tareas registradas.")
 
 def mostrar_tarea(task):
     estado = Fore.GREEN + "✅ Completada" if task.status else Fore.RED + "❌ No completada"
     cat = task.category if task.category else Fore.RED + "Sin categoria";
-    print(Fore.CYAN + f"🆔 {task.id} - {task.title} - Usuario: {task.user}: descripcion: {task.description}, categoria: [{cat}], prioridad: {task.priority}, fecha de creación [{task.createDate}], fecha de edición [{task.lastEditDate}], [{estado}].")
+    print(Fore.CYAN + f"🆔 {task.id} - {task.title} - Usuario: {task.user}: descripcion: {task.description}, categoria: [{cat}], prioridad: {task.priority}, fecha de creación [{task.createDate.strftime("%Y-%m-%d %H:%M:%S")}], fecha de edición [{task.lastEditDate.strftime("%Y-%m-%d %H:%M:%S")}], [{estado}].")
     
 def limpiar_pantalla():
     os.system ('cls' if os.name == 'nt' else 'clear')
